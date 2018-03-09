3d24f0114fb0d9abf9a609597085be04e90a02f2
TobiasSuhrborg <tobias.suhrborg@it-objects.de>
2018-03-09 12:51:33 +0100

    added additional exception handling, for unknown error reason and added detailed logging

--- src/main/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReader.java
+++ src/main/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReader.java
@@ -2,9 +2,19 @@ package de.ito.gradle.plugin.androidstringextractor.internal;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -12,6 +22,7 @@ import org.xml.sax.SAXException;
 
 class StringValuesReader {
   private XmlFileReader xmlFileReader;
+  private Logger logger = Logger.getAnonymousLogger();
 
   StringValuesReader(XmlFileReader xmlFileReader) {
     this.xmlFileReader = xmlFileReader;
@@ -39,12 +50,33 @@ class StringValuesReader {
   private Map<String, String> resolveStringValues(NodeList strings) {
     Map<String, String> values = new LinkedHashMap<>();
     for (int i = 0; i < strings.getLength(); i++) {
-      handleNode(strings.item(i), values);
+      try {
+        handleNode(strings.item(i), values);
+        throw new RuntimeException ("test");
+      }catch(RuntimeException e){
+          logNodeHandlingException(strings.item(i), e);
+      }
     }
     return values;
   }
 
-  private void handleNode(Node stringNode, Map<String, String> values) {
+    private void logNodeHandlingException(Node node, RuntimeException e)  {
+        try {
+            logger.log(Level.SEVERE,"an unexpected error occurred while reading string_layouts.\n entry '"+convertNodeToText(node)+"' will not be considered",e);
+        } catch (TransformerException e1) {
+            logger.log(Level.SEVERE,"an unexpected error occurred while reading string_layouts.\n entry will not be considered",e);
+        }
+    }
+
+    private String convertNodeToText(Node node )throws TransformerException {
+        Transformer t = TransformerFactory.newInstance().newTransformer();
+        t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter sw = new StringWriter();
+        t.transform(new DOMSource(node), new StreamResult(sw));
+        return sw.toString();
+    }
+
+    private void handleNode(Node stringNode, Map<String, String> values) {
     Node stringNodeNameAttribute = stringNode.getAttributes().getNamedItem("name");
     Node stringNodeValueAttribute = stringNode.getFirstChild();
     if (stringNodeNameAttribute != null && stringNodeValueAttribute != null) {
