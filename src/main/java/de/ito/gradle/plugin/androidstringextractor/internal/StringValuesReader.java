dd6b044a7a5017e20b7420540201fb2b7e51c6d6
Tobias.Suhrborg <tobias.suhrborg@it-objects.de>
2018-03-14 18:01:29 +0100

    Bugfix/invalid string value format (#6)
    * created test to check for handling of <string name="xyz"/> (no value)
    
    * implemented reader class to ignore empty node values
    
    * added additional exception handling, for unknown error reason and added detailed logging
    
    * incremented patch
    
    * changed method order to be more readable

--- src/main/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReader.java
+++ src/main/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReader.java
@@ -2,9 +2,19 @@ package de.ito.gradle.plugin.androidstringextractor.internal;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -12,26 +22,27 @@ import org.xml.sax.SAXException;
 
 class StringValuesReader {
   private XmlFileReader xmlFileReader;
+  private Logger logger = Logger.getAnonymousLogger();
 
   StringValuesReader(XmlFileReader xmlFileReader) {
     this.xmlFileReader = xmlFileReader;
   }
 
   StringValues read(File flavorPath)
-      throws ParserConfigurationException, SAXException, IOException {
+          throws ParserConfigurationException, SAXException, IOException {
     File stringValuesFile = new File(flavorPath, "res/values/string_layouts.xml");
 
     return resolveStringValues(stringValuesFile);
   }
 
   private StringValues resolveStringValues(File stringValuesFile)
-      throws ParserConfigurationException, SAXException, IOException {
+          throws ParserConfigurationException, SAXException, IOException {
     if (!stringValuesFile.exists()) return new StringValues();
 
     Document document = xmlFileReader.read(stringValuesFile);
     NodeList strings = document.getElementsByTagName("string");
 
-    Map<String,String> values = resolveStringValues(strings);
+    Map<String, String> values = resolveStringValues(strings);
 
     return new StringValues(values);
   }
@@ -39,11 +50,36 @@ class StringValuesReader {
   private Map<String, String> resolveStringValues(NodeList strings) {
     Map<String, String> values = new LinkedHashMap<>();
     for (int i = 0; i < strings.getLength(); i++) {
-      Node string = strings.item(i);
-      String key = string.getAttributes().getNamedItem("name").getNodeValue();
-      String value = string.getFirstChild().getNodeValue();
-      values.put(key, value);
+      try {
+        handleNode(strings.item(i), values);
+      }catch(RuntimeException e){
+          logNodeHandlingException(strings.item(i), e);
+      }
     }
     return values;
   }
+
+    private void handleNode(Node stringNode, Map<String, String> values) {
+        Node stringNodeNameAttribute = stringNode.getAttributes().getNamedItem("name");
+        Node stringNodeValueAttribute = stringNode.getFirstChild();
+        if (stringNodeNameAttribute != null && stringNodeValueAttribute != null) {
+            values.put(stringNodeNameAttribute.getNodeValue(), stringNodeValueAttribute.getNodeValue());
+        }
+    }
+
+    private void logNodeHandlingException(Node node, RuntimeException e)  {
+        try {
+            logger.log(Level.SEVERE,"an unexpected error occurred while reading string_layouts.\n entry '"+convertNodeToText(node)+"' will not be considered");
+        } catch (TransformerException e1) {
+            logger.log(Level.SEVERE,"an unexpected error occurred while reading string_layouts.\n entry will not be considered");
+        }
+    }
+
+    private String convertNodeToText(Node node )throws TransformerException {
+        Transformer t = TransformerFactory.newInstance().newTransformer();
+        t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+        StringWriter sw = new StringWriter();
+        t.transform(new DOMSource(node), new StreamResult(sw));
+        return sw.toString();
+    }
 }
--- src/test/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReaderTest.java
+++ src/test/java/de/ito/gradle/plugin/androidstringextractor/internal/StringValuesReaderTest.java
@@ -1,6 +1,7 @@
 package de.ito.gradle.plugin.androidstringextractor.internal;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import javax.xml.parsers.ParserConfigurationException;
@@ -21,7 +22,7 @@ import static org.mockito.Mockito.when;
 
 public class StringValuesReaderTest {
   @Rule public TemporaryFolder folder = new TemporaryFolder();
-  
+
   private StringValuesReader stringValuesReader;
   private XmlFileReader xmlFileReader;
 
@@ -31,15 +32,24 @@ public class StringValuesReaderTest {
     stringValuesReader = new StringValuesReader(xmlFileReader);
   }
 
-  @Test
-  public void when_readStringValuesFromFile_should_returnStringValues() throws Exception {
+  @Test public void when_readStringValuesFromFile_should_returnStringValues() throws Exception {
     StringValues expected = createDummyStringValues();
     Document dummyStringValues = createDummyDocument();
     when(xmlFileReader.read(any(File.class))).thenReturn(dummyStringValues);
-    File flavorPath = new File(folder.getRoot(), "flavor/");
-    File stringValuesFile = new File(flavorPath, "res/values/string_layouts.xml");
-    stringValuesFile.mkdirs();
-    stringValuesFile.createNewFile();
+    File flavorPath = createFileStructure();
+
+    StringValues actual = stringValuesReader.read(flavorPath);
+
+    assertThat(actual, equalTo(expected));
+  }
+
+  @Test
+  public void given_invalidFormatNode_when_readStringValuesFromFile_should_returnStringValues_andIgnoreInvalidNode()
+          throws Exception {
+    StringValues expected = createDummyStringValues();
+    Document dummyStringValues = createDummyDocumentWithInvalidNode();
+    when(xmlFileReader.read(any(File.class))).thenReturn(dummyStringValues);
+    File flavorPath = createFileStructure();
 
     StringValues actual = stringValuesReader.read(flavorPath);
 
@@ -58,6 +68,15 @@ public class StringValuesReaderTest {
     Document document = Util.createEmptyDocument();
 
     Element resources = document.createElement("resources");
+    Element string = createStringNodeEntry(document);
+
+    resources.appendChild(string);
+    document.appendChild(resources);
+
+    return document;
+  }
+
+  private static Element createStringNodeEntry(Document document) {
     Element string = document.createElement("string");
     Attr name = document.createAttribute("name");
     name.setValue("name");
@@ -65,10 +84,31 @@ public class StringValuesReaderTest {
 
     string.setAttributeNode(name);
     string.appendChild(value);
+    return string;
+  }
 
-    resources.appendChild(string);
-    document.appendChild(resources);
+  private File createFileStructure() throws IOException {
+    File flavorPath = new File(folder.getRoot(), "flavor/");
+    File stringValuesFile = new File(flavorPath, "res/values/string_layouts.xml");
+    stringValuesFile.mkdirs();
+    stringValuesFile.createNewFile();
+    return flavorPath;
+  }
+
+  static Document createDummyDocumentWithInvalidNode() throws ParserConfigurationException {
+    Document document = createDummyDocument();
+
+    document.getElementsByTagName("resources").item(0).appendChild(createStringNodeEntryWithoutValue(document));
 
     return document;
+  }
+
+  private static Element createStringNodeEntryWithoutValue(Document document) {
+    Element string = document.createElement("string");
+    Attr name = document.createAttribute("name");
+    name.setValue("withoutValue");
+
+    string.setAttributeNode(name);
+    return string;
   }
 }
